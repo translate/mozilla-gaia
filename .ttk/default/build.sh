9bf604af767d3fc9df91463b7d2ea6369653100b
Khaled Hosny <khaledhosny@eglug.org>
2013-06-27 16:58:30 +0200

    Initialize Mercurial repositories in ttk-build

--- .ttk/default/build.sh
+++ .ttk/default/build.sh
@@ -4,19 +4,37 @@ source ttk.inc.sh
 
 langs=$(which_langs $*)
 
+function update_hg_repo()
+{
+	(
+	mkdir -p $base_dir/build/locales/
+	cd $base_dir/build/locales/
+	if [ -d $mozlang ]; then
+		cd $mozlang;
+		hg pull;
+		hg update;
+	else
+		hg clone http://hg.mozilla.org/gaia-l10n/$mozlang $mozlang
+	fi
+	)
+}
+
 for lang in $langs
 do
 	log_info "Building: $lang"
 	if [ "$lang" == "templates" ]; then
+		polang=$lang
+		mozlang=en-US
 		# Update en-US
-		(cd $base_dir/build/locales/en-US; hg pull; hg update)
+		update_hg_repo
 		
 		# Make new template files
-		rm $(find $translation_dir/templates -name "*.pot")
-		(cd $base_dir/build/locales/en-US; moz2po $verbosity --exclude=".hgtags" --exclude="*.diff" -P . $translation_dir/templates)
+		rm $(find $translation_dir/$polang -name "*.pot")
+		(cd $base_dir/build/locales/$mozlang; moz2po $verbosity --exclude=".hgtags" --exclude="*.diff" -P . $translation_dir/$polang)
 	else
 		polang=$(get_language_pootle $lang)
 		mozlang=$(get_language_upstream $lang)
+		update_hg_repo
 		# update against templates
 		pot2po $verbosity -t $translation_dir/$polang $translation_dir/templates $translation_dir/$polang
 		# new locale files
