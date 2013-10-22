9fc4a3db3ae7daf34ebf48e26969167ae0a2fbe8
Dwayne Bailey <dwayne@translate.org.za>
2013-10-22 13:10:02 +0100

    Prepare for hosting v1.1

---
+++ .ttk/firefoxos-1.1/config.sh
@@ -0,0 +1,10 @@
+release=1.1
+project=firefoxos_$release
+instance=mozilla
+user=pootlesync
+server=pootle.locamotion.org
+local_copy=$base_dir/.pootle_tmp
+manage_command="/var/www/sites/$instance/src/manage.py"
+manage_py_verbosity=2
+precommand=". /var/www/sites/$instance/env/bin/activate;"
+opt_verbose=3
