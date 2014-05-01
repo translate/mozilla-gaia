7f5a5ca066fa76a6b8b24da0b40548816b2d68ff
Dwayne Bailey <dwayne@translate.org.za>
2014-05-01 15:18:34 +0100

    Tools: add 1.4 config

---
+++ .ttk/firefoxos-1.4/config.sh
@@ -0,0 +1,10 @@
+release=1.4
+project=firefoxos_$release
+instance=mozilla
+user=pootlesync
+server=mozilla.locamotion.org
+local_copy=$base_dir/.pootle_tmp_1_4
+manage_command="/var/www/sites/$instance/src/manage.py"
+manage_py_verbosity=2
+precommand=". /var/www/sites/$instance/env/bin/activate;"
+opt_verbose=3
