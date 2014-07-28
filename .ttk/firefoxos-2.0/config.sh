785c358a698c202d2bc44b33c890a449e18f961a
Dwayne Bailey <dwayne@translate.org.za>
2014-07-28 15:03:01 +0100

    Tools: enable 2.0 config

---
+++ .ttk/firefoxos-2.0/config.sh
@@ -0,0 +1,10 @@
+release=2.0
+project=firefoxos_$release
+instance=mozilla
+user=pootlesync
+server=mozilla.locamotion.org
+local_copy=$base_dir/.pootle_tmp_2_0
+manage_command="/var/www/sites/$instance/src/manage.py"
+manage_py_verbosity=2
+precommand=". /var/www/sites/$instance/env/bin/activate;"
+opt_verbose=3
