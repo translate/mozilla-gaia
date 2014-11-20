2dd6ad2e9ac41be94bd5db0c060e6747d2c81d67
Mozilla Pootle L10n Robot <tech-team@translate.org.za>
2014-11-20 10:17:37 +0000

    Config: reduce verbosity

--- .ttk/firefoxos-2.1/config.sh
+++ .ttk/firefoxos-2.1/config.sh
@@ -5,8 +5,8 @@ user=pootlesync
 server=mozilla.locamotion.org
 local_copy=$base_dir/.pootle_tmp_2_1
 manage_command="/var/www/sites/$instance/src/manage.py"
-manage_py_verbosity=2
+manage_py_verbosity=0
 precommand=". /var/www/sites/$instance/env/bin/activate;"
-opt_verbose=3
+opt_verbose=0
 
 alt_src="fr ru"
